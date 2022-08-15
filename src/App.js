f97711a8f67d718efbbbe7b0186973354af1f09a
Evan Walter <evan.walter.software@gmail.com>
2022-08-15 15:58:35 -0700

    style: implement all responsive styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './index.css'
 export default function App() {
   return (
     <>
-      <div className='text-3xl font-bold underline'>Hello, world!</div>
+      <div className='px-10'>
+        <div className='flex flex-col items-center justify-between max-w-2xl pb-10 mx-auto my-20 border-b border-gray-300 xs:flex-row'>
+          <button className='p-4 mb-5 text-white transition bg-green-500 rounded-full hover:scale-105 xs:mb-0'>
+            Get A New Random Joke
+          </button>
+          <a href='#' className='text-blue-500 underline hover:text-blue-600'>
+            View API Docs
+          </a>
+        </div>
+        <p className='font-bold text-center text-gray-500 uppercase'>
+          Loading your joke...
+        </p>
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      xs: '420px',
+    },
     extend: {},
   },
   plugins: [],
